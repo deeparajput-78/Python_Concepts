a6579db795736f5190832aefc8d12028c79379a8
Deepa Rajput <251839642+deeparajput-78@users.noreply.github.com>
2026-07-12 00:53:21 +0530

    Initial Commit

---
+++ Variables.py
@@ -0,0 +1,13 @@
+# Taking input from the user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height (in cm): "))
+course = input("Enter your course: ")
+
+# Displaying the output
+print("\n----- Student Information -----")
+print("Name   :", name)
+print("Age    :", age)
+print("Height :", height, "cm")
+print("Course :", course)
+
