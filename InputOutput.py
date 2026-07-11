9f0c808372ff19f6f6c7c82fc24afb0ac5e80b46
Deepa Rajput <251839642+deeparajput-78@users.noreply.github.com>
2026-07-12 01:06:22 +0530

    Add InputOutput.py

---
+++ InputOutput.py
@@ -0,0 +1,15 @@
+
+# Taking input from the user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+semester = int(input("Enter your semester: "))
+cgpa = float(input("Enter your CGPA: "))
+
+# Displaying the information
+print("\n Student Information")
+print(f"Name      : {name}")
+print(f"Age       : {age}")
+print(f"Course    : {course}")
+print(f"Semester  : {semester}")
+print(f"CGPA      : {cgpa}")
