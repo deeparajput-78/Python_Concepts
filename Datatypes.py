61bb5374f4ed3cdc7ce14ed4da5507898bd59400
Deepa Rajput <251839642+deeparajput-78@users.noreply.github.com>
2026-07-12 00:56:41 +0530

    Add Datatypes.py

---
+++ Datatypes.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height (in cm): "))
+course = input("Enter your course: ")
+
+
+
+# Displaying data types
+print("\n----- Data Types -----")
+print("Type of name   :", type(name))
+print("Type of age    :", type(age))
+print("Type of height :", type(height))
+print("Type of course :", type(course))
