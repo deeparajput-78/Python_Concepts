a9c3cd8c9c220960ae5d9316bf44ac9acccf5f8a
Deepa Rajput <251839642+deeparajput-78@users.noreply.github.com>
2026-07-12 22:56:08 +0530

    Add conditionalstatement.py

---
+++ ConditionalStatement.py
@@ -0,0 +1,8 @@
+# Program to check voting eligibility using only if statement
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("your not eligible")
